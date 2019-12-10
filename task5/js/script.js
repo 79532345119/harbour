cf5e2230756f4276d8257f7f82edf19b73963f49
Mikhail <l_mikhail@inbox.ru>
2019-12-10 17:13:20 +0300

    Money Keeper final

---
+++ task5/js/script.js
@@ -0,0 +1,17 @@
+let menu = document.querySelectorAll('.menu');
+console.log(menu);
+console.log(menu[0]);
+let menuItem = document.querySelectorAll('.menu-item');
+console.log(menuItem);
+//console.log(menuItem[0]);
+//.insertBefore(menuItem[2], menuItem[1]);
+
+//menuItem.insertBefore(menuItem[2], menuItem[1]);
+//menu[0].insertBefore(menuItem[2], menuItem[1]);
+//let menuItemNew = document.getElementsByTagName('li');
+//console.log(menuItemNew);
+//menuItem.insertBefore(menuItem[2], menuItem[1]);
+
+
+
+//document.body.insertBefore(menu[2], menu[1]);
---
+++ test.js
@@ -0,0 +1,9 @@
+
+answer = prompt("ок?");
+while(answer == "" || answer == null) {
+    
+ alert("not ok");
+ answer = prompt("ок?");
+}
+console.log(answer);
+console.log(typeof(answer));
